89816b78b02d6f108d3dc3fe62c60dd9542b1f84
hardev1010 <hardevs1010@gmail.com>
2024-09-27 20:48:35 +0530

    spacing and minor spelling correction

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
 const videoSchema = new Schema(
     {
         videoFile: {
@@ -22,7 +23,7 @@ const videoSchema = new Schema(
             type: Number, 
             required: true
         },
-        viewa: {
+        view: {
             type: Number,
             default: 0
         },
